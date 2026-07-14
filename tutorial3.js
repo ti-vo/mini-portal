314d0bbaa8fb774c7c253c2ec23cf3f2c46423a9
ti-vo <teresa.vogl@uni-leipzig.de>
2026-07-14 12:05:24 +0200

    updated tutorial 3

--- tutorial3.js
+++ tutorial3.js
@@ -15,3 +15,56 @@ console.log(anzahlGelesen)
 
 const { title, author } = books[0]; 
 console.log(title, author)
+
+
+class Buch {
+  constructor(title, author, year) {
+    this.title = title;
+    this.author = author;
+    this.year = year;
+    this.read = false;
+  }
+
+  markAsRead() {
+    this.read = true;
+  }
+
+  description() {
+    return `${this.title} by ${this.author} (${this.year})`;
+  }
+}
+
+class Bibliothek {
+  constructor() {
+    this.buecher = [];
+  }
+
+  buchHinzufuegen(buch) {
+    this.buecher.push(buch);
+  }
+
+  ungeleseneBuecher() {
+    return this.buecher.filter((buch) => !buch.read);
+  }
+
+  anzahlBuecher() {
+    return this.buecher.length;
+  }
+}
+
+
+const meineBibliothek = new Bibliothek();
+
+const buch1 = new Buch("Brave New World", "Aldous Huxley", 1932);
+const buch2 = new Buch("Das Blut der Anderen", "Simone de Beauvoir", 1945);
+const buch3 = new Buch("1984", "George Orwell", 1949);
+
+buch1.markAsRead(); 
+buch2.markAsRead(); 
+
+meineBibliothek.buchHinzufuegen(buch1);
+meineBibliothek.buchHinzufuegen(buch2);
+meineBibliothek.buchHinzufuegen(buch3);
+
+console.log("Anzahl Bücher:", meineBibliothek.anzahlBuecher());
+console.log("Ungelesene Bücher:", meineBibliothek.ungeleseneBuecher());
